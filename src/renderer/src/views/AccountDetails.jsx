17f332266226450b7c1013929b42690321977fa5
Grace Lee <gracelee9901@gmail.com>
2024-04-13 16:35:22 -0700

    Remove unnecessary code and import statements

--- src/renderer/src/views/AccountDetails.jsx
+++ src/renderer/src/views/AccountDetails.jsx
@@ -1,17 +1,7 @@
-import { useState, useEffect } from "react";
-
-export default function AccountDetails({accountPathKey}) {
-  const [pathAgain, setPathAgain] = useState(accountPathKey);
-
-  useEffect(() => {
-    setPathAgain(accountPathKey);  // Update local state to force re-render
-    console.log("Effect runs: New ID set", accountPathKey);
-}, [accountPathKey]);
-
+export default function AccountDetails() {
   return (
     <>
       <span>Account Details page</span>
-      <span>{pathAgain}</span>
     </>
   );
 }
